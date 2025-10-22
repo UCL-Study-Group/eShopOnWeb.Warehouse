b7187278831b79a9a10bc88d6882cea8531fe3bb
TheSecondSoda <thomas.lind.spil@gmail.com>
2025-10-22 16:36:59 +0200

    Made PostgresRepo and added attribute to BaseModel

--- Warehouse.Common/Models/Base/BaseModel.cs
+++ Warehouse.Common/Models/Base/BaseModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Warehouse.Common.Models.Base
+﻿using RepoDb.Attributes;
+
+namespace Warehouse.Common.Models.Base
 {
     public abstract class BaseModel
     {
+        [Primary]
         public required string Id { get; set; }
     }
 }
